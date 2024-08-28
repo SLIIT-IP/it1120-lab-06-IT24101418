d46bef883b6dea9c09f7f7d1418f07b45b5d4169
IT24101418 <IT24101418@my.sliit.lk>
2024-08-28 12:32:36 +0530

    Add files via upload

---
+++ Q2/IT24101418Lab6Q2A.java
@@ -0,0 +1,13 @@
+public class IT24101418Lab6Q2B
+{
+  public static void main(String [] args)
+   {
+        int number = 1;
+    
+     while (number <= 10) {
+         System.out.print( number+" " );
+         number++;
+        
+      }
+    }
+}
